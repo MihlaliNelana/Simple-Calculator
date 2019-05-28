c36499abac33ea48e0fdaa6cb38dfe3c452da42c
MihlaliNelana <mihlali.nelana@umuzi.org>
2019-05-28 17:05:52 +0200

    subtraction function commit

--- jasmine-standalone-3.4.0_2/spec/CalculatorSpec.js
+++ jasmine-standalone-3.4.0_2/spec/CalculatorSpec.js
@@ -1,16 +1,17 @@
 describe("Calculator", function(){
+    let calculator = new Calculator();
     describe("add", function () {
     
         it("should be able to add two numbers", function() {
-            result = add(1,3);
+            result = calculator.add(1,3);
             expect(result).toBe(4);
         });
     
         it("should be able to add multiple numbers together", function() {
-            result = add(1,2,4,5);
+            result = calculator.add(1,2,4,5);
             expect(result).toBe(12);
     
-            result = add(5,0,0,5);
+            result = calculator.add(5,0,0,5);
             expect(result).toEqual(10);
         });
     });
@@ -18,13 +19,26 @@ describe("Calculator", function(){
     describe("multiply", function() {
         
         it("should be able to multiply two numbers", function() {
-            result = multiply(1,2);
+            result = calculator.multiply(1,2);
             expect(result).toEqual(2);
         });
     
         it("should be able to multiply multiple numbers", function(){
-            result = multiply(1,2,3,4);
+            result = calculator.multiply(1,2,3,4);
             expect(result).toEqual(24)
+        });
+    });
+
+    describe("subtract", function(){
+        
+        it("should be able to subtract two numbers from each other", function(){
+            result = calculator.subtract(10,2);
+            expect(result).toEqual(8);
+        });
+
+        it("should be able to subtract multiple numbers", function(){
+            result = calculator.subtract(5,1,1);
+            expect(result).toEqual(3);
         });
     });
     
--- jasmine-standalone-3.4.0_2/src/Calculator.js
+++ jasmine-standalone-3.4.0_2/src/Calculator.js
@@ -1,15 +1,27 @@
-function add(){
-    var result = 0;
-    for(var i = 0; i < arguments.length; i++){
-        result += parseFloat(arguments[i]);
-    }
-    return result;
-}
+function Calculator(){
 
-function multiply(){
-    var result = 1;
-    for(var j = 0; j < arguments.length; j++){
-        result *= parseFloat(arguments[j]);
-    }
-    return result;
+    this.add = function(){
+        var result = 0;
+        for(var i = 0; i < arguments.length; i++){
+            result += parseFloat(arguments[i]);
+        }
+        return result;
+    };
+    
+    this.multiply = function(){
+        var result = 1;
+        for(var j = 0; j < arguments.length; j++){
+            result *= parseFloat(arguments[j]);
+        }
+        return result;
+    };
+    
+    this.subtract = function(){
+        for(var i = 0; i < arguments.length; i++){
+            var result = arguments[i];
+            result -= parseFloat(arguments[i++]);
+        }
+        return result;
+    };
+    
 }
