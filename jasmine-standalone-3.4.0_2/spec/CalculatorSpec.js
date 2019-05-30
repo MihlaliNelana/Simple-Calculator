477e8471662ea8294e8947cf162a9e3e18d7ddc6
MihlaliNelana <mihlali.nelana@umuzi.org>
2019-05-30 10:38:06 +0200

    morning commit

--- jasmine-standalone-3.4.0_2/spec/CalculatorSpec.js
+++ jasmine-standalone-3.4.0_2/spec/CalculatorSpec.js
@@ -3,43 +3,41 @@ describe("Calculator", function(){
     describe("add", function () {
     
         it("should be able to add two numbers", function() {
-            result = calculator.add(1,3);
-            expect(result).toBe(4);
+            result = calculator.add(1,3.5);
+            expect(result).toBe(4.5);
+
+            result = calculator.add(5.5,1,2);
+            expect(result).toBe(8.5);
         });
     
         it("should be able to add multiple numbers together", function() {
             result = calculator.add(1,2,4,5);
             expect(result).toBe(12);
     
-            result = calculator.add(5,0,0,5);
-            expect(result).toEqual(10);
+            result = calculator.add(5.5,0,0,5);
+            expect(result).toEqual(10.5);
         });
     });
     
     describe("multiply", function() {
         
         it("should be able to multiply two numbers", function() {
+            
             result = calculator.multiply(1,2);
             expect(result).toEqual(2);
+
+            result = calculator.multiply(4,2);
+            expect(result).toEqual(8);
         });
     
         it("should be able to multiply multiple numbers", function(){
+            
             result = calculator.multiply(1,2,3,4);
-            expect(result).toEqual(24)
+            expect(result).toEqual(24);
+
+            result = calculator.multiply(6,2,1);
+            expect(result).toEqual(12);
+
         });
     });
-
-    describe("subtract", function(){
-        
-        it("should be able to subtract two numbers from each other", function(){
-            result = calculator.subtract(10,2);
-            expect(result).toEqual(8);
-        });
-
-        it("should be able to subtract multiple numbers", function(){
-            result = calculator.subtract(5,1,1);
-            expect(result).toEqual(3);
-        });
-    });
-    
 });
--- jasmine-standalone-3.4.0_2/src/Calculator.js
+++ jasmine-standalone-3.4.0_2/src/Calculator.js
@@ -1,27 +1,21 @@
-function Calculator(){
-
-    this.add = function(){
-        var result = 0;
-        for(var i = 0; i < arguments.length; i++){
-            result += parseFloat(arguments[i]);
-        }
-        return result;
-    };
-    
-    this.multiply = function(){
-        var result = 1;
-        for(var j = 0; j < arguments.length; j++){
-            result *= parseFloat(arguments[j]);
-        }
-        return result;
-    };
-    
-    this.subtract = function(){
-        for(var i = 0; i < arguments.length; i++){
-            var result = arguments[i];
-            result -= parseFloat(arguments[i++]);
-        }
-        return result;
-    };
-    
+class Calculator {
+    constructor() {
+        this.add = function (last) {
+            last = new last();
+            var result = 0;
+            for (var i = 0; i < arguments.length; i++) {
+                result += parseFloat(arguments[i]);
+            }
+            return result;
+        };
+        
+        this.multiply = function(last){
+            last = new last();
+            var result = 1;
+            for(var j = 0; j < arguments.length; j++){
+                result *= parseFloat(arguments[j]);
+            }
+            return result;
+        };
+    }
 }
